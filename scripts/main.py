82c2aebed54ae150148feb24ff24e4abf0157404
Samuel Atkins <s.m.atkins73@gmail.com>
2021-03-18 15:20:27 -0400

    18/03/2021-Atkins-15:20:27 Removed Jaimungal environment invocation.

--- scripts/main.py
+++ scripts/main.py
@@ -1,16 +1,5 @@
 from src.ddpg import DDPG
-from src.environments.jaimungal_environment import JaimungalEnvironment
 
 if __name__ == "__main__":
-
-    # jenv = JaimungalEnvironment()
-
-    # while jenv.t < jenv.N - 1:
-    #     jenv.v[jenv.t] = jenv.get_optimal()
-    #     jenv.step()
-    # jenv.plot_simulation()
-
     ddpg = DDPG()
     ddpg.run_ddpg()
-
-    print("Hello World!")
