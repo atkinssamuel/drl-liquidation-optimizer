ff19d040a873a2cd2ed3bc5c45704223c95b2631
Samuel Atkins <s.m.atkins73@gmail.com>
2021-03-25 12:01:16 -0400

    25/03/2021-Atkins-12:01:16 Completed analytical solution comparison function. Will now create custom DDPG module and try to achieve convergence to analytical solution.

--- src/constants.py
+++ src/constants.py
@@ -4,17 +4,29 @@ class Directories:
     """
     # ddpg results
     ddpg_results = "ddpg-results/"
-    ddpg_is_results = ddpg_results + "is/"
     ddpg_loss_results = ddpg_results + "losses/"
     ddpg_is_ma_results = ddpg_results + "is-ma/"
     ddpg_reward_results = ddpg_results + "rewards/"
-    ddpg_sim = ddpg_results + "sim/"
+    ddpg_model_inv_results = ddpg_results + "model-inv/"
 
+
+    # custom ddpg results
+    custom_ddpg_results = "custom-ddpg-results/"
+    
 
 class DDPGHyperparameters:
     D = 5
     lr = 0.3
     batch_size = 1024
+    M = 400
+    criticLR = 0.01
+    actorLR = 0.01
+
+
+class CustomDDPGHyperparameters:
+    lr = 0.3
+    batch_size = 1024
     M = 200
     criticLR = 0.01
     actorLR = 0.01
+
